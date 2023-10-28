f760a1e2f31a16299b82eca6f9aa01c28a6874dc
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-28 18:12:56 -0300

    Criei context para modelagem do banco de dados

--- Agenda.API/Data/ContactContext.cs
+++ Agenda.API/Data/ContactContext.cs
@@ -2,11 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Agenda.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Agenda.API.Data
 {
-    public class ContactContext
+    public class ContactContext : DbContext
     {
-        
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var contact = modelBuilder.Entity<Contact>();
+            contact.ToTable("contacts");
+            contact.HasKey(attribute => attribute.Id);
+            contact.Property(attribute => attribute.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            contact.Property(attribute => attribute.Name).HasColumnName("name").IsRequired();
+            contact.Property(attribute => attribute.LastName).HasColumnName("last_name");
+            contact.Property(attribute => attribute.Phone).HasColumnName("phone").IsRequired();
+            contact.Property(attribute => attribute.Email).HasColumnName("email");
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

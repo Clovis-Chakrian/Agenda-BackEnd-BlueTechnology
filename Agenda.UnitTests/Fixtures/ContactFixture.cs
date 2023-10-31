68e4f97d25f897f51e4423055876f2355e38e0c5
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-31 11:29:51 -0300

    Feat: created a contact dto fixture to use at tests

--- Agenda.UnitTests/Fixtures/ContactFixture.cs
+++ Agenda.UnitTests/Fixtures/ContactFixture.cs
@@ -2,37 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Agenda.API.Dtos;
 using Agenda.API.Models;
 
 namespace Agenda.UnitTests.Fixtures
 {
     public static class ContactFixture
     {
-        public static List<Contact> GetListOfTestContacts() =>
-        new()
+        private static readonly DateTime _date = DateTime.Now;
+        public static List<ContactDto> GetListOfTestContacts()
         {
-            new Contact {
-                Id = 1,
-                Name = "Clóvis",
-                LastName = "Chakrian",
-                Phone = "(81) 90000-0000",
-                Email = "clovischakrian@gmail.com"
-            },
-            new Contact {
-                Id = 2,
-                Name = "João",
-                LastName = "Chakrian",
-                Phone = "(81) 91111-1111",
-                Email = ""
-            },
-            new Contact {
-                Id = 3,
-                Name = "Hilária",
-                LastName = "Chakrian",
-                Phone = "(81) 92222-2222",
-                Email = "hilariachakrian@gmail.com"
-            },
-        };
+            return new()
+            {
+                new ContactDto {
+                    Id = 1,
+                    Name = "Clóvis",
+                    LastName = "Chakrian",
+                    Phone = "(81) 90000-0000",
+                    Email = "clovischakrian@gmail.com",
+                },
+                new ContactDto {
+                    Id = 2,
+                    Name = "João",
+                    LastName = "Chakrian",
+                    Phone = "(81) 91111-1111",
+                    Email = "",
+                },
+                new ContactDto {
+                    Id = 3,
+                    Name = "Hilária",
+                    LastName = "Chakrian",
+                    Phone = "(81) 92222-2222",
+                    Email = "hilariachakrian@gmail.com",
+                },
+            };
+        }
 
         public static Contact GetTestContact() =>
         new()
@@ -41,7 +45,17 @@ namespace Agenda.UnitTests.Fixtures
             Name = "Clóvis",
             LastName = "Chakrian",
             Phone = "(81) 90000-0000",
-            Email = "clovischakrian@gmail.com"
+            Email = "clovischakrian@gmail.com",
+        };
+
+        public static ContactDto GetTestContactDto() =>
+        new()
+        {
+            Id = 1,
+            Name = "Clóvis",
+            LastName = "Chakrian",
+            Phone = "(81) 90000-0000",
+            Email = "clovischakrian@gmail.com",
         };
     }
 }

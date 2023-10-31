51b57614ed39fbf4d0db17d24443a8ed2e844ba8
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-31 10:16:20 -0300

    Feat: added created at and updated at attributes

--- Agenda.API/Models/Contact.cs
+++ Agenda.API/Models/Contact.cs
@@ -3,9 +3,11 @@ namespace Agenda.API.Models
     public class Contact
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set;}
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
     }
 }

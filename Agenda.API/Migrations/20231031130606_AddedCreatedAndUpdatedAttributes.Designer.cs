9ba98e02008a7c777e07e50deaad21d4747a7676
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-31 10:15:48 -0300

    Feat: created migrations with updated at and created at to update the table

--- Agenda.API/Migrations/20231031130606_AddedCreatedAndUpdatedAttributes.Designer.cs
+++ Agenda.API/Migrations/20231031130606_AddedCreatedAndUpdatedAttributes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Agenda.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Agenda.API.Migrations
 {
     [DbContext(typeof(ContactContext))]
-    partial class ContactContextModelSnapshot : ModelSnapshot
+    [Migration("20231031130606_AddedCreatedAndUpdatedAttributes")]
+    partial class AddedCreatedAndUpdatedAttributes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Agenda.API/Migrations/20231031130606_AddedCreatedAndUpdatedAttributes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agenda.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCreatedAndUpdatedAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "last_name",
+                table: "contacts",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "contacts",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_at",
+                table: "contacts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "contacts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_at",
+                table: "contacts");
+
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "contacts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "last_name",
+                table: "contacts",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "contacts",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}

605fc7be03aed673c55cc94d839417b8407c3b06
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-11-01 19:48:14 -0300

    Feat: the lastname field is now required

--- Agenda.API/Libs/ContactValidation.cs
+++ Agenda.API/Libs/ContactValidation.cs
@@ -8,10 +8,11 @@ namespace Agenda.API.Libs
 {
     public class ContactValidation
     {
-        public static void Validate(Contact contact) 
+        public static void Validate(Contact contact)
         {
             if (contact == null) throw new ArgumentException("");
             if (contact.Name == null || contact.Name == "") throw new ArgumentException("O campo nome é obrigatório e não pode ser uma string vazia.");
+            if (contact.LastName == null || contact.LastName == "") throw new ArgumentException("O sobrenome é obrigatório e não pode ser uma string vazia.");
             if (!PhoneValidation.IsValid(contact.Phone) || contact.Phone == "") throw new ArgumentException("O telefone recebido não é válido! Os telefones cadastrados devem seguir o padrão (81) 90000-0000 de telefones do Brasil.");
             if ((contact.Email == "" || contact.Email != null) && !EmailValidation.IsValid(contact.Email)) throw new ArgumentException("O email não é um campo obrigatório, mas ao ser inserido deve ser um email válido!");
         }

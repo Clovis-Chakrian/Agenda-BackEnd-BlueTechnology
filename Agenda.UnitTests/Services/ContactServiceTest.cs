742af4435db1ed850cfccd37e53a3c0e5d2dcbe4
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-31 23:43:55 -0300

    Test: covered the update method

--- Agenda.UnitTests/Services/ContactServiceTest.cs
+++ Agenda.UnitTests/Services/ContactServiceTest.cs
@@ -145,5 +145,101 @@ namespace Agenda.UnitTests.Services
             // Assert
             mockContactRepository.Verify(service => service.SearchContact(ContactFixture.GetTestContact().Id), Times.Once());
         }
+
+        [Fact]
+        public async Task SearchContactByName_OnSuccess_ReturnsListOfDtoContacts()
+        {
+            // Arrange
+            var mockContactRepository = new Mock<IContactRepository>();
+            mockContactRepository.Setup(service => service.SearchByName(ContactFixture.GetTestContact().Name, ContactFixture.GetTestContact().LastName)).ReturnsAsync(ContactFixture.GetListOfPureModelTestContacts());
+            var contactService = new ContactService(mockContactRepository.Object, AutoMapperHelper.AutoMapper());
+
+            // Act
+            var result = await contactService.SearchContactByName(ContactFixture.GetTestContact().Name, ContactFixture.GetTestContact().LastName);
+
+            // Assert
+            result.Should().BeEquivalentTo(ContactFixture.GetListOfTestContacts());
+        }
+
+        [Fact]
+        public async Task SearchContactByName_OnSuccess_InvolkesContactRepositoryExactlyOnce()
+        {
+            // Arrange
+            var mockContactRepository = new Mock<IContactRepository>();
+            mockContactRepository.Setup(service => service.SearchByName(ContactFixture.GetTestContact().Name, ContactFixture.GetTestContact().LastName)).ReturnsAsync(ContactFixture.GetListOfPureModelTestContacts());
+            var contactService = new ContactService(mockContactRepository.Object, AutoMapperHelper.AutoMapper());
+
+            // Act
+            var result = await contactService.SearchContactByName(ContactFixture.GetTestContact().Name, ContactFixture.GetTestContact().LastName);
+
+            // Assert
+            mockContactRepository.Verify(service => service.SearchByName(ContactFixture.GetTestContact().Name, ContactFixture.GetTestContact().LastName), Times.Once());
+        }
+
+        [Fact]
+        public async Task SearchContactByName_OnSuccessButNoMatchs_ReturnsEmptyListOfDtoContacts()
+        {
+            // Arrange
+            var mockContactRepository = new Mock<IContactRepository>();
+            mockContactRepository.Setup(service => service.SearchByName(ContactFixture.GetTestContact().Name, ContactFixture.GetTestContact().LastName)).ReturnsAsync(new List<Contact>());
+            var contactService = new ContactService(mockContactRepository.Object, AutoMapperHelper.AutoMapper());
+
+            // Act
+            var result = await contactService.SearchContactByName(ContactFixture.GetTestContact().Name, ContactFixture.GetTestContact().LastName);
+
+            // Assert
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Update_OnSuccess_ReturnsTrue()
+        {
+            // Arrange
+            var mockContactRepository = new Mock<IContactRepository>();
+            mockContactRepository.Setup(service => service.SearchContact(ContactFixture.GetTestContact().Id)).ReturnsAsync(ContactFixture.GetTestContact());
+            mockContactRepository.Setup(service => service.UpdateContact(ContactFixture.GetTestContact()));
+            mockContactRepository.Setup(service => service.SaveChangesAsync()).ReturnsAsync(true);
+            var contactService = new ContactService(mockContactRepository.Object, AutoMapperHelper.AutoMapper());
+
+            // Act
+            var result = await contactService.UpdateContact(ContactFixture.GetTestContactDto().Id, ContactFixture.GetTestContactDto());
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Update_OnFails_ReturnsFalse()
+        {
+            // Arrange
+            var mockContactRepository = new Mock<IContactRepository>();
+            mockContactRepository.Setup(service => service.SearchContact(ContactFixture.GetTestContact().Id)).ReturnsAsync((Contact)null);
+            mockContactRepository.Setup(service => service.UpdateContact(ContactFixture.GetTestContact()));
+            mockContactRepository.Setup(service => service.SaveChangesAsync()).ReturnsAsync(false);
+            var contactService = new ContactService(mockContactRepository.Object, AutoMapperHelper.AutoMapper());
+
+            // Act
+            var result = await contactService.UpdateContact(ContactFixture.GetTestContactDto().Id, ContactFixture.GetTestContactDto());
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Update_OnSucces_InvolkesContactRepositoryExactlyTwice()
+        {
+            // Arrange
+            var mockContactRepository = new Mock<IContactRepository>();
+            mockContactRepository.Setup(service => service.SearchContact(ContactFixture.GetTestContact().Id)).ReturnsAsync(ContactFixture.GetTestContact());
+            mockContactRepository.Setup(service => service.UpdateContact(ContactFixture.GetTestContact()));
+            mockContactRepository.Setup(service => service.SaveChangesAsync()).ReturnsAsync(true);
+            var contactService = new ContactService(mockContactRepository.Object, AutoMapperHelper.AutoMapper());
+
+            // Act
+            var result = await contactService.UpdateContact(ContactFixture.GetTestContactDto().Id, ContactFixture.GetTestContactDto());
+
+            // Assert
+            mockContactRepository.Verify(service => service.SaveChangesAsync(), Times.Once());
+        }
     }
 }

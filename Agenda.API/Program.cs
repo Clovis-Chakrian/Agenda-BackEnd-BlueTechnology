4373af574927752ddfdff039c4231c954c7cc872
Clóvis Chakrian <clovischakriandev@gmail.com>
2024-08-24 17:08:27 -0300

    feat: adicionando docker file e fazendo upgrade da versão do dotnet

--- Agenda.API/Program.cs
+++ Agenda.API/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Agenda.API.Data;
 using Agenda.API.Repository;
 using Agenda.API.Services;
@@ -5,13 +6,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+var DB_USER = Environment.GetEnvironmentVariable("DB_USER") ?? throw new Exception("Env variable DB_USER");
+var DB_PASSWORD = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? throw new Exception("Env variable DB_PASSWORD");
+var DB_HOST = Environment.GetEnvironmentVariable("DB_HOST") ?? throw new Exception("Env variable DB_HOST");
+var DB_PORT = Environment.GetEnvironmentVariable("DB_PORT") ?? throw new Exception("Env variable DB_PORT");
+var DB_DATABASE = Environment.GetEnvironmentVariable("DB_DATABASE") ?? throw new Exception("Env variable DB_DATABASE");
+
+var connectionString = $"User id={DB_USER}; Password={DB_PASSWORD}; Host={DB_HOST}; Port={DB_PORT}; Database={DB_DATABASE}";
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ContactContext>(options => {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
+    options.UseNpgsql(connectionString);
 });
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
 builder.Services.AddScoped<IContactService, ContactService>();
